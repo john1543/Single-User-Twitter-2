e2c45a566979d31c0e13a1fed88796af21f0c267
Wally Glutton <stungeye@gmail.com>
2013-11-07 13:06:59 -0600

    Initial commit of a default Rails app. Soon to be twitter.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = '1b066bae03de93c2b5f5d6f783af1f7d7af3c1e148854fc4a9aeffb7e63a0ba8e94153c6a8fe0583c2493edeebf054c8f1142452a12f5f94f99e2237b349e740'
